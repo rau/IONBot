d7eb5f9847130c1edd35eaad358303237f1c5988
Raunak <raunakdaga@gmail.com>
2019-05-24 08:50:16 -0400

    Major mac changes

--- main.py
+++ main.py
@@ -21,51 +21,13 @@ IDtonames['762'] = 'Biotec'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def createGUI():
-    root = Tk()
-    root.title('IONBot')
-    root.geometry('210x180')
-    root.configure(background='white')
-
-    username = Entry(root, width=20)
-    userlabel = Label(root, text = 'Username:', background='white')
-    password = Entry(root, show="*", width=20)
-    passwordlabel = Label(root, text = 'Password:', background='white')
-    userlabel.place(x = 10, y = 10)
-    username.place(x = 70, y = 10)
-    passwordlabel.place(x = 10, y = 35)
-    password.place(x = 70, y = 35)
-    username.focus()
-
-    classlabel = Label(root, text='Activity:', background='white')
-    classlabel.place(x = 10, y = 60)
-    classCombo = Combobox(root)
-    classCombo['values'] = ('Volleyball', 'Weight Room', 'Library Study Hall', 'Biotech')
-    classCombo.place(x = 62, y = 60)
-
-    customClassLabel = Label(root, text='Custom:', background='white')
-    customClassLabel.place(x = 10, y=85)
-    customClassInfo = Entry(root, width=20)
-    customClassInfo.place(x=70, y = 85)
-
-    selected = IntVar()
-    rad1 = Radiobutton(root, text='A', value=1, variable=selected)
-    rad2 = Radiobutton(root, text='B', value=2, variable=selected)
-    rad1.place(x = 76, y = 110)
-    rad2.place(x = 116, y = 110)
-
-    doProcess = Button(root, text="Begin Searching", command=beginProcess)
-    doProcess.place(x = 65, y = 135)
-
-    status_label = Label(root, text = 'Not Searching', background='white')
-    status_label.place(x = 75, y = 160)
-
-    root.mainloop()
 
 def beginProcess():
-    status_label.configure(text='Searching') # Not Working
+    status_label.configure(text='Searching')  # Not Working
     root.update_idletasks()
-    signup(username.get(), password.get(), classToID.get(classCombo.get()), selected.get(), customClassInfo.get())
+    signup(username.get(), password.get(), classToID.get(
+        classCombo.get()), selected.get(), customClassInfo.get())
+
 
 def getBlockText(driver, blockRequested):
     current_day = driver.find_element_by_class_name('current-day')
@@ -78,14 +40,16 @@ def getBlockText(driver, blockRequested):
     blockWanted = blockElement.find_element_by_class_name('selected-activity')
     return blockWanted
 
+
 def ifCustomGetName(driver, customID):
     driver.get('https://ion.tjhsst.edu/eighth/?activity=' + customID)
     className = driver.find_element_by_class_name('activity-detail-link')
     return className.text
 
+
 def signup(username, password, activity, blockRequested, customID):
     driver = webdriver.Chrome(dir_path + r'\chromedriver.exe')
-    driver.get('https://ion.tjhsst.edu/') # Go to ion
+    driver.get('https://ion.tjhsst.edu/')  # Go to ion
 
     # Gets Past Login Page
     id_box = driver.find_element_by_name('username')
@@ -97,7 +61,7 @@ def signup(username, password, activity, blockRequested, customID):
 
     if 'Upcoming' not in driver.page_source:
         status_label.configure(text='Incorrect Login')
-        return 'Terminated' # Does nothing
+        return 'Terminated'  # Does nothing
 
     # Gets Past 8th Period Page
     eighth = driver.find_element_by_xpath('/html/body/div[3]/ul/li[2]/a')
@@ -143,5 +107,44 @@ def signup(username, password, activity, blockRequested, customID):
     driver.close()
 
 
+root = Tk()
+root.title('IONBot')
+root.geometry('210x180')
+root.configure(background='white')
+
+username = Entry(root, width=20)
+userlabel = Label(root, text='Username:', background='white')
+password = Entry(root, show="*", width=20)
+passwordlabel = Label(root, text='Password:', background='white')
+userlabel.place(x=10, y=10)
+username.place(x=70, y=10)
+passwordlabel.place(x=10, y=35)
+password.place(x=70, y=35)
+username.focus()
+
+classlabel = Label(root, text='Activity:', background='white')
+classlabel.place(x=10, y=60)
+classCombo = Combobox(root)
+classCombo['values'] = ('Volleyball', 'Weight Room', 'Library Study Hall', 'Biotech')
+classCombo.place(x=62, y=60)
+
+customClassLabel = Label(root, text='Custom:', background='white')
+customClassLabel.place(x=10, y=85)
+customClassInfo = Entry(root, width=20)
+customClassInfo.place(x=70, y=85)
+
+selected = IntVar()
+rad1 = Radiobutton(root, text='A', value=1, variable=selected)
+rad2 = Radiobutton(root, text='B', value=2, variable=selected)
+rad1.place(x=76, y=110)
+rad2.place(x=116, y=110)
+
+doProcess = Button(root, text="Begin Searching", command=beginProcess)
+doProcess.place(x=65, y=135)
+
+status_label = Label(root, text='Not Searching', background='white')
+status_label.place(x=75, y=160)
+
+root.mainloop()
 
 beginProcess()
